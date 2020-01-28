50fe521d479ec4cdd152a8bccee36d580852d254
jojos38 <jojos38000@hotmail.com>
2020-01-28 11:33:10 +0100

    Revert "c"
    This reverts commit 66fa203eb6d22d024e3f27f50937b4bc7ea1613f.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 //
 // Created by Антон on 28.01.2020.
 //
-#include "main.h"
-#include <iostream>
-using namespace std;
+int main() {
 
-int main(int argc, char** argv) {
-    return 0;
 }
+#include "main.h"
--- src/main.h
+++ src/main.h
@@ -7,7 +7,6 @@
 
 
 class main {
-	
 
 };
 
