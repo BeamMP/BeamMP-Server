435c397d020b98ae6ff6fecd0d2cbacc2cda3264
Anonymous275 <theanonymous275@gmail.com>
2020-12-20 15:14:23 +0200

    Small edit

--- src/Network/Websocket.cpp
+++ src/Network/Websocket.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Anonymous275.
+// Copyright (c) 2019-present Anonymous275.
 // BeamMP Server code is not in the public domain and is not free software.
 // One must be granted explicit permission by the copyright holder in order to modify or distribute any part of the source or binaries.
 // Anything else is prohibited. Modified works may not be published and have be upstreamed to the official repository.
