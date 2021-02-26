5ded0e2a6d7497e0e55118c8430505f288fbdd62
Lion Kortlepel <development@kortlepel.com>
2021-02-26 22:29:50 +0100

    AddNewCar: use insert with make_unique again

--- src/Client.cpp
+++ src/Client.cpp
@@ -34,7 +34,7 @@ int TClient::GetOpenCarID() const {
 }
 
 void TClient::AddNewCar(int Ident, const std::string& Data) {
-    mVehicleData.emplace(new TVehicleData { Ident, Data });
+    mVehicleData.insert(std::make_unique<TVehicleData>(Ident, Data));
 }
 
 TClient::TSetOfVehicleData& TClient::GetAllCars() {
