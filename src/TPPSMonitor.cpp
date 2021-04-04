bec698fbdc68f6f6a8c4388abf03be8b090c032f
Lion Kortlepel <development@kortlepel.com>
2021-04-04 02:57:25 +0200

    temporary fix for timeout during sync

--- src/TPPSMonitor.cpp
+++ src/TPPSMonitor.cpp
@@ -44,7 +44,7 @@ void TPPSMonitor::operator()() {
                 V += c->GetCarCount();
             }
             // kick on "no ping"
-            if (c->SecondsSinceLastPing() > 30) {
+            if (c->SecondsSinceLastPing() > (5 * 60)) {
                 debug("client " + std::string("(") + std::to_string(c->GetID()) + ")" + c->GetName() + " timing out: " + std::to_string(c->SecondsSinceLastPing()) + ", pps: " + Application::PPS());
                 TimedOutClients.push_back(c);
             }
@@ -52,7 +52,7 @@ void TPPSMonitor::operator()() {
             return true;
         });
         for (auto& ClientToKick : TimedOutClients) {
-            Network().ClientKick(*ClientToKick, "Timeout (no ping for >30 seconds)");
+            Network().ClientKick(*ClientToKick, "Timeout (no ping for >5 min)");
         }
         TimedOutClients.clear();
         if (C == 0 || mInternalPPS == 0) {
