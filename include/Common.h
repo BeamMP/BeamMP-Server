9595ef164e2e37f10f4c50254cc4d7f9bc16b38a
Lion Kortlepel <development@kortlepel.com>
2021-07-11 20:41:23 +0200

    use fake version for lua update for now

--- include/Common.h
+++ include/Common.h
@@ -51,6 +51,7 @@ public:
     static void GracefullyShutdown();
     static TConsole& Console() { return *mConsole; }
     static std::string ServerVersion() { return "2.2.0"; }
+#warning "change version from 2.2.0 to real version"
     static std::string ClientVersion() { return "2.0"; }
     static std::string PPS() { return mPPS; }
     static void SetPPS(std::string NewPPS) { mPPS = NewPPS; }
@@ -120,4 +121,3 @@ void RegisterThread(const std::string str);
 #define Biggest 30000
 std::string Comp(std::string Data);
 std::string DeComp(std::string Compressed);
-
