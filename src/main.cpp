943159cd40902a657671770f07d9136e695ef1bf
Lion Kortlepel <development@kortlepel.com>
2021-07-11 20:34:18 +0200

    Lua: add onShutdown

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,7 @@ int main(int argc, char** argv) try {
 
     bool Shutdown = false;
     Application::RegisterShutdownHandler([&Shutdown] { Shutdown = true; });
+    Application::RegisterShutdownHandler([] { TriggerLuaEvent("onShutdown", false, nullptr, {}, true); });
 
     TServer Server(argc, argv);
     TConfig Config;
