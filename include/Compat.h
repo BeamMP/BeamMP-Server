a8ece2688ab70b5b8afe55858ee8875ed0cf9105
Lion Kortlepel <development@kortlepel.com>
2021-08-12 11:47:38 +0200

    possible windows compiler fix, again

--- include/Compat.h
+++ include/Compat.h
@@ -7,10 +7,6 @@
 #include <sys/socket.h>
 #include <termios.h>
 #include <unistd.h>
-using SOCKET = int;
-using DWORD = unsigned long;
-using PDWORD = unsigned long*;
-using LPDWORD = unsigned long*;
 char _getch();
 inline void CloseSocketProper(int TheSocket) {
     shutdown(TheSocket, SHUT_RDWR);
@@ -21,6 +17,8 @@ inline void CloseSocketProper(int TheSocket) {
 // ======================= WIN32 =======================
 
 #ifdef WIN32
+#include <conio.h>
+#include <winsock2.h>
 inline void CloseSocketProper(SOCKET TheSocket) {
     shutdown(TheSocket, 2); // 2 == SD_BOTH
     closesocket(TheSocket);
