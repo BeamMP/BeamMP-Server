9efe352e7a45f2479a64414dd99fc703178c841d
Lion Kortlepel <development@kortlepel.com>
2021-09-20 15:41:40 +0300

    Lua: Working MP.CreateEventTimer

--- src/TLuaEngine.cpp
+++ src/TLuaEngine.cpp
@@ -547,7 +547,7 @@ void TLuaEngine::StateThreadData::operator()() {
                 }
             }
         }
-        std::this_thread::sleep_for(std::chrono::microseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
 
