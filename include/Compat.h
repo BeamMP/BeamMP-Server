c56d99b209e0a39c2bc035dd2c1cefba812e9a81
Lucca Jiménez Könings <lucca@Mac-mini-von-Lucca.local>
2021-11-25 01:00:24 +0100

    Add temporary cmake mods to make project compile on macOS, add ifdefs in 'Compat.h' for target darwin

--- include/Compat.h
+++ include/Compat.h
@@ -19,6 +19,23 @@ inline void CloseSocketProper(int TheSocket) {
 }
 #endif // unix
 
+#ifdef __APPLE__
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+using SOCKET = int;
+using DWORD = unsigned long;
+using PDWORD = unsigned long*;
+using LPDWORD = unsigned long*;
+char _getch();
+inline void CloseSocketProper(int TheSocket) {
+    shutdown(TheSocket, SHUT_RDWR);
+    close(TheSocket);
+}
+#endif // unix
+
 // ======================= WIN32 =======================
 
 #ifdef WIN32
@@ -33,6 +50,6 @@ inline void CloseSocketProper(SOCKET TheSocket) {
 
 // ======================= OTHER =======================
 
-#if !defined(WIN32) && !defined(__unix)
+#if !defined(WIN32) && !defined(__unix) && !defined(__APPLE__)
 #error "OS not supported"
 #endif
