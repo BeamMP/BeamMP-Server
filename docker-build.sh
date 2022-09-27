b88b64cd0e957aba3d61bd145d2f6042830063f8
Lion Kortlepel <development@kortlepel.com>
2022-09-28 00:01:09 +0200

    update docker-build.sh to add an option to run-only

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+# usage:
+# ./docker-build.sh
+#       builds the image, and then runs it
+# ./docker-build.sh run
+#       only runs it
+
 set -e
 
-git submodule update --init --recursive
+if [ "$1" != "run" ]
+then
+    git submodule update --init --recursive
+    docker build . --tag beammp-static
+fi
 
-docker build . --tag beammp-static
 
 docker run -v $(pwd):/root -it --rm beammp-static sh -c "cd root; cmake . -B build -DGIT_SUBMODULE=OFF -DLUA_INCLUDE_DIR=/lua/src -DSOL2_SINGLE=ON -DFIND_OPENSSL=OFF -DOPENSSL_CRYPTO=\"/usr/lib/libcrypto.a\" -DOPENSSL_SSL=\"/usr/lib/libssl.a\" -DFIND_ZLIB=OFF -DZLIB_ZLIB=/lib/libz.a -DCURL_FOUND=ON -DCURL_LIBRARIES=\"/usr/lib/libcurl.a\" -DLUA_LIBRARIES=\"/lua/src/liblua.a\" -DSENTRY_BACKEND=none -DSENTRY_TRANSPORT=none; make -C build -j BeamMP-Server"
+
+# try to chown the build directory afterwards
+sudo chown -R "$USER":"$USER" build
 
