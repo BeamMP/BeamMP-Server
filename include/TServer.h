69726a9b03f8b344c61cd7f9a20cf5a35864ddc0
20dka <doni@doni.hu>
2022-11-04 23:40:52 +0100

    add owner check to other vehicle packets

--- include/TServer.h
+++ include/TServer.h
@@ -44,6 +44,7 @@ private:
     static bool IsUnicycle(TClient& c, const std::string& CarJson);
     static void Apply(TClient& c, int VID, const std::string& pckt);
     static bool HandlePosition(TClient& c, const std::string& Packet);
+    static bool HandleVehicleUpdate(TClient& c, const std::string& Packet);
 };
 
 struct BufferView {
