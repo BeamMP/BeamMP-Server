3df71e96c5a68e6fb08ce3a908acc3db55c9f632
Luuk van Oijen <lazyluuk.channel@gmail.com>
2023-11-27 12:39:15 +0100

    adding vid to the vehicle spawn data

--- src/server/car.rs
+++ src/server/car.rs
@@ -18,7 +18,7 @@ pub struct Car {
 impl Car {
     pub fn new(car_json: String) -> Self {
         Self {
-            car_json: car_json,
+            car_json,
 
             ..Default::default()
         }
