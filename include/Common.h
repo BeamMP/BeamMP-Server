7f206fd0d4caee0953a72787edf7c89460c5cf95
Lion Kortlepel <development@kortlepel.com>
2023-12-03 17:49:12 +0100

    bump version to 3.1.2

--- include/Common.h
+++ include/Common.h
@@ -137,7 +137,7 @@ private:
     static inline std::mutex mShutdownHandlersMutex {};
     static inline std::deque<TShutdownHandler> mShutdownHandlers {};
 
-    static inline Version mVersion { 3, 1, 1 };
+    static inline Version mVersion { 3, 1, 2 };
 };
 
 std::string ThreadName(bool DebugModeOverride = false);
