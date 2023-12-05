98673069f23af47c0daa2f657602c3a36da2c88a
Lion Kortlepel <development@kortlepel.com>
2023-12-05 14:06:32 +0100

    remove cat from configure step

--- scripts/debian-11/2-configure.sh
+++ scripts/debian-11/2-configure.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-cmake . -B bin -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-O3 -s -Wl,-z,norelro -Wl,--hash-style=gnu -Wl,--build-id=none -Wl,-z,noseparate-code -ffunction-sections -fdata-sections -Wl,--gc-sections" -DBeamMP-Server_ENABLE_LTO=ON || cat "$SOURCE"/server/bin/vcpkg-bootstrap.log
+cmake . -B bin $1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-O3 -s -Wl,-z,norelro -Wl,--hash-style=gnu -Wl,--build-id=none -Wl,-z,noseparate-code -ffunction-sections -fdata-sections -Wl,--gc-sections" -DBeamMP-Server_ENABLE_LTO=ON
