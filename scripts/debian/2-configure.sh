8ab018485b32faebab9443f48c5d4ee06f20fd30
Lion Kortlepel <development@kortlepel.com>
2023-12-05 13:29:59 +0100

    add back recursive fetch

--- scripts/debian/2-configure.sh
+++ scripts/debian/2-configure.sh
@@ -8,4 +8,10 @@ then
     git clone https://github.com/lionkor/commandline/ deps/commandline
 fi
 
+if [ -ne ./deps/commandline/CMakeLists.txt ]
+then
+    rm -rf deps/commandline
+    git clone https://github.com/microsoft/vcpkg/ deps/commandline
+fi
+
 cmake ${1:-.} -B bin -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-O3 -s -Wl,-z,norelro -Wl,--hash-style=gnu -Wl,--build-id=none -Wl,-z,noseparate-code -ffunction-sections -fdata-sections -Wl,--gc-sections" -DBeamMP-Server_ENABLE_LTO=ON || cat "$SOURCE"/server/bin/vcpkg-bootstrap.log
