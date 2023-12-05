96ce0e05a129a52db6f7b702b1573e5b54f1b49e
Lion Kortlepel <development@kortlepel.com>
2023-12-05 13:45:13 +0100

    add git safe

--- scripts/debian/1-install-deps.sh
+++ scripts/debian/1-install-deps.sh
@@ -4,6 +4,4 @@ set -ex
 
 apt-get update -y
 
-apt-get install -y liblua5.3-0 liblua5.3-dev
-apt-get install -y curl zip unzip tar
-apt-get install -y cmake make git g++
+apt-get install -y liblua5.3-0 liblua5.3-dev curl zip unzip tar cmake make git g++
