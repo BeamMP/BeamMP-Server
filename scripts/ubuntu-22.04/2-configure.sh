ba080d05a22694b096e4288a99b899dc9134d78a
Lion <development@kortlepel.com>
2023-12-30 13:56:39 +0100

    add arm64 builds to github actions
    Update linux.yml to build ARM64 binaries for debian11
    
    update linux.yml to fix incorrect runs-on tags
    
    add ubuntu 22.04 arm64 build
    
    Update linux.yml
    
    Update linux.yml
    
    Update linux.yml
    
    Update linux.yml
    
    Update linux.yml
    
    Update linux.yml
    
    Update 2-configure.sh
    
    Update 2-configure.sh
    
    Update 1-install-deps.sh
    
    Update 1-install-deps.sh
    
    Update 2-configure.sh
    
    Update 2-configure.sh
    
    Update linux.yml
    
    use get-cmake
    
    update vcpkg
    
    force arm64 triplet

--- scripts/ubuntu-22.04/2-configure.sh
+++ scripts/ubuntu-22.04/2-configure.sh
@@ -2,4 +2,6 @@
 
 set -ex
 
+./vcpkg/bootstrap-vcpkg.sh
+
 cmake . -B bin $1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-O3 -g -Wl,-z,norelro -Wl,--hash-style=gnu -Wl,--build-id=none -Wl,-z,noseparate-code -ffunction-sections -fdata-sections -Wl,--gc-sections" -DBeamMP-Server_ENABLE_LTO=ON
