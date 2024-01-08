a70b6031f9f8a7a86b5499ea67f8083b34de4cd8
Lion Kortlepel <development@kortlepel.com>
2024-01-08 12:15:24 +0100

    fix bug in HandlePosition which caused the vehicle position not to be saved properly

--- include/TServer.h
+++ include/TServer.h
@@ -43,7 +43,7 @@ private:
     static bool ShouldSpawn(TClient& c, const std::string& CarJson, int ID);
     static bool IsUnicycle(TClient& c, const std::string& CarJson);
     static void Apply(TClient& c, int VID, const std::string& pckt);
-    static void HandlePosition(TClient& c, const std::string& Packet);
+    void HandlePosition(TClient& c, const std::string& Packet);
 };
 
 struct BufferView {
