b04c5068ea6f681da68f2ce1ebdd8089c985c81a
Lion Kortlepel <development@kortlepel.com>
2024-10-05 16:09:02 +0200

    bump version

--- include/Common.h
+++ include/Common.h
@@ -127,7 +127,7 @@ private:
     static inline std::mutex mShutdownHandlersMutex {};
     static inline std::deque<TShutdownHandler> mShutdownHandlers {};
 
-    static inline Version mVersion { 3, 5, 1 };
+    static inline Version mVersion { 3, 6, 0 };
 };
 
 void SplitString(std::string const& str, const char delim, std::vector<std::string>& out);
