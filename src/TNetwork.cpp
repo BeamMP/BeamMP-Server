5c39216c44ef153c35809cb9734ed71673a13c7d
Tixx <83774803+WiserTixx@users.noreply.github.com>
2026-08-14 02:27:22 +0200

    Make log more readable

--- src/TNetwork.cpp
+++ src/TNetwork.cpp
@@ -201,7 +201,8 @@ void TNetwork::UDPServerMain() {
                             beammp_debugf("Invalid size for UDP value. IP: {} ID: {}, Size: {}", remote_client_ep.address().to_string(), ID, Data.size());
                             std::stringstream hexData;
                             for (auto c : Data) {
-                                hexData << std::hex << std::setw(2) << std::setfill('0') << c;
+                                hexData << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c);
+                                hexData << " ";
                             }
                             beammp_debug(hexData.str());
                             return false;
